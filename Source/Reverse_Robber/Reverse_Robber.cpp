f397d7fd04cfdbc6473164b6ee6e6cffb2896cb2
Shawn <mdzzsb233@gmail.com>
2024-07-09 19:49:28 -0600

    Revert "Add C++ functionality"
    This reverts commit 73093e3fc0b8fa8483b066f8f1e3c2884fa1d6d0.

--- Source/Reverse_Robber/Reverse_Robber.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Reverse_Robber.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Reverse_Robber, "Reverse_Robber" );
