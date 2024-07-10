73093e3fc0b8fa8483b066f8f1e3c2884fa1d6d0
Shawn <mdzzsb233@gmail.com>
2024-07-09 19:17:27 -0600

    Add C++ functionality

---
+++ Source/Reverse_Robber/Reverse_Robber.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Reverse_Robber.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Reverse_Robber, "Reverse_Robber" );
